25d342628fe8c578f39ddc36aad6221e5c1dcb67
IvchenkoEV <pesok72@mail.ru>
2026-02-28 10:43:30 +0500

    Добавлен контроллер

---
+++ src/main/java/aston/controller/UserController.java
@@ -0,0 +1,72 @@
+package aston.controller;
+
+import aston.dto.CreateUserRequestDto;
+import aston.dto.UpdateUserRequestDto;
+import aston.dto.UserResponseDto;
+import aston.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody CreateUserRequestDto requestDto) {
+        log.info("Создание пользователя: {}", requestDto.getEmail());
+        UserResponseDto responseDto = userService.createUser(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        log.info("Поиск пользователя по ID: {}", id);
+        UserResponseDto responseDto = userService.findUserById(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<UserResponseDto> getUserByEmail(@PathVariable String email) {
+        log.info("Поиск пользователя по Email: {}", email);
+        UserResponseDto responseDto = userService.findUserByEmail(email);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        log.info("Запрос всех пользователей");
+        List<UserResponseDto> usersList = userService.findAllUsers();
+        return ResponseEntity.ok(usersList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(
+            @PathVariable Long id,
+            @RequestBody UpdateUserRequestDto requestDto) {
+        log.info("Обновление пользователя: ID={}", id);
+        UserResponseDto responseDto = userService.updateUser(id, requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        log.info("Удаление пользователя: ID={}", id);
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
